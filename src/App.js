1a0ccd9f0062a01caf8fa8abfdf1ce0b775f98b9
Jeremey Lee <leejeremey@gmail.com>
2019-08-13 15:30:09 -0700

    passed props through routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navigation from './components/navigation';
 import Home from './components/home';
 import Login from './components/login';
 import Signup from './components/signup';
+import Posts from './components/posts';
 
 const App = (props) => {
   const { Header, Footer } = Layout;
@@ -22,6 +23,21 @@ const App = (props) => {
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
+          <Route 
+            exact path="/general-discussion" 
+            render={(props) => <Posts {...props} boardName='General Discussion' />} />
+            <Route 
+            exact path="/memes" 
+            render={(props) => <Posts {...props} boardName='Memes' />} />
+            <Route 
+            exact path="/crying-cat" 
+            render={(props) => <Posts {...props} boardName='Crying Cat Pictures' />} />
+            <Route 
+            exact path="/morbid-reality" 
+            render={(props) => <Posts {...props} boardName='Morbid Reality' />} />
+            <Route 
+            exact path="/off-topic" 
+            render={(props) => <Posts {...props} boardName='Off Topic' />} />
           <Row type="flex" justify="center">
             <Footer style= {{position: 'absolute', bottom:'0'}}>
               Forum ©2019 Created by Jeremey & Kyle
